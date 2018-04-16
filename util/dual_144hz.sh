b940ccaa3560fabf11d343ec2f37029300a7f1f5
Scott <MabezDev@gmail.com>
2018-04-16 22:20:57 +0000

    Fixed CSET for cpu isolation, Added s cript to disable primary monitor when a GPU is passed through, modified the 144hz script to retry until the monitor is plugged in

--- util/dual_144hz.sh
+++ util/dual_144hz.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --rate 144 --output HDMI-0 --off
+
+until xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --rate 144 --output HDMI-0 --off
+do
+  sleep 1
+done
+
+# Finally restart i3 as it gets bugged adding and removing monitors
+i3-msg restart	
