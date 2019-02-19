19136a572ca214461071cc3885801b025fd167d4
Scott Mabin <MabezDev@gmail.com>
2019-02-19 23:10:23 +0000

    VFIO/BORG:
    	Borg:
    	- Dont backup programs, only keep 5 dailies
    	VFIO:
    	- Up pages to 12GB for hugepages
    	- Fix VM IP
    	- and ram defrag before huge page allocation
    	- Fix perf gov when vm is shutdown

--- util/dual_144hz.sh
+++ util/dual_144hz.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-until xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --rate 144 --output HDMI-0 --off
+until xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --rate 144 --output HDMI-0 --off
 do
   sleep 1
 done
--- vfio/huge_pages.sh
+++ vfio/huge_pages.sh
@@ -3,7 +3,7 @@
 ON=$1
 if [ "$ON" == "1" ]; then
 	mount /hugepages
-	sysctl vm.nr_hugepages=4096
+	sysctl vm.nr_hugepages=6144
 else
 	sysctl vm.nr_hugepages=0
 	umount /hugepages
