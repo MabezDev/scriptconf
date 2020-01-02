e7d4d9d0fbf14f35a6207c65c43f9512a6a55d92
Scott Mabin <scott@mabez.dev>
2020-01-02 21:26:57 +0000

    Fixed borg backup, updated kernel config

--- vfio/huge_pages.sh
+++ vfio/huge_pages.sh
@@ -2,9 +2,9 @@
 
 ON=$1
 if [ "$ON" == "1" ]; then
-	mount /hugepages
+	# mount /hugepages
 	sysctl vm.nr_hugepages=6144
 else
 	sysctl vm.nr_hugepages=0
-	umount /hugepages
+	# umount /hugepages
 fi
