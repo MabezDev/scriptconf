fe21689cdb18d33c505028ac019ee32a1ac0bda1
Scott Mabin <scott@mabez.dev>
2020-05-26 12:02:52 +0100

    fmt

--- vfio/libvirt/hooks/utils/perf-release.sh
+++ vfio/libvirt/hooks/utils/perf-release.sh
@@ -4,10 +4,11 @@
 source "/etc/libvirt/hooks/kvm.conf"
 
 sysctl vm.stat_interval=1
-sysctl -w kernel.watchdog=1
+
 echo always > /sys/kernel/mm/transparent_hugepage/enabled
 echo always >/sys/kernel/mm/transparent_hugepage/defrag
-echo ondemand | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+
 echo 1 > /sys/bus/workqueue/devices/writeback/numa
+
 echo $TOTAL_CORES_MASK > /sys/bus/workqueue/devices/writeback/cpumask
 echo $TOTAL_CORES_MASK > /sys/devices/virtual/workqueue/cpumask
--- vfio/libvirt/hooks/utils/perf-start.sh
+++ vfio/libvirt/hooks/utils/perf-start.sh
@@ -16,12 +16,13 @@ fi
 
 # Reduce VM jitter: https://www.kernel.org/doc/Documentation/kernel-per-CPU-kthreads.txt
 sysctl vm.stat_interval=120
-sysctl -w kernel.watchdog=0
+
 # the kernel's dirty page writeback mechanism uses kthread workers. They introduce
 # massive arbitrary latencies when doing disk writes on the host and aren't
 # migrated by cset. Restrict the workqueue to use only cpu 0.
 echo $HOST_CORES_MASK > /sys/bus/workqueue/devices/writeback/cpumask
 echo $HOST_CORES_MASK > /sys/devices/virtual/workqueue/cpumask
+
 # THP can allegedly result in jitter. Better keep it off.
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 echo never >/sys/kernel/mm/transparent_hugepage/defrag
