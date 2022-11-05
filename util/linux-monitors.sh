08609e37932c9710380f16f3baf308329e82792a
Scott Mabin <scott@mabez.dev>
2022-11-05 18:33:22 +0000

    Updates for 3rd monitor

--- util/linux-monitors.sh
+++ util/linux-monitors.sh
@@ -4,8 +4,7 @@
 # gigabyte
 ddcutil -b "4" setvcp 60 "0x0f"
 ddcutil -b "5" setvcp 60 "0x0f"
-# benq
-ddcutil -b "2" setvcp 60 "0x03"
+ddcutil -b "6" setvcp 60 "0x0f"
 
 # setup displays
-xrandr --output DisplayPort-1 --set TearFree on --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal --output DisplayPort-0 --set TearFree on --mode 2560x1440 --rate 165 --pos 2560x0 --rotate normal --output DisplayPort-2 --set TearFree on --primary --mode 1920x1080 --rate 60 --pos 5120x0 --rotate normal
+xrandr --output DisplayPort-0 --set TearFree on --mode 2560x1440 --rate 165 --pos 0x0 --rotate normal --output DisplayPort-1 --set TearFree on --mode 2560x1440 --rate 165 --pos 2560x0 --rotate normal --output DisplayPort-2 --set TearFree on --primary --mode 2560x1440 --rate 165 --pos 5120x0 --rotate normal
